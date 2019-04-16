21459afac62c0442ba86339bd72e273e296f4617
wcjb <wcjbyjx@gmail.com>
2019-04-16 10:38:55 +0800

    添加Plotly Express注释

--- Code/Plotly.py
+++ Code/Plotly.py
@@ -1,3 +1,6 @@
+
+
+# Plotly Express使用
 #%%
 import plotly_express as px
 print(px.data.iris.__doc__)
@@ -15,5 +18,4 @@ px.scatter(iris, x="sepal_width", y="sepal_length", color="species")
 px.scatter(iris, x="sepal_width", y="sepal_length", color="species", marginal_y="violin", marginal_x="rug")
 
 #%%
-px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent", 
-           hover_name="country", log_x=True, size_max=60)
+px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent", hover_name="country", log_x=True, size_max=60)
