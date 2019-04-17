29eae401650b6a227ea313bd9ae6a20d9402c671
wcjb <wcjbyjx@gmail.com>
2019-04-17 11:55:14 +0800

    增加Dash中关于布局部分的实例

---
+++ Learning/DashLayout.py
@@ -0,0 +1,31 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='第一季度销售分析'),
+
+    html.Div(children='''
+        门店不含成都华置精品店、深圳CoCoPark店，预算不含2019新店.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': '测试数据'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
